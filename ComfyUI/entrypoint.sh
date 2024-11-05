4f80fb8803e3a8c06f54f21e10d915724f02945e
lapaliv <lapaliv@gmail.com>
2024-11-05 09:30:43 +0100

    docker

---
+++ ComfyUI/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+vram_mode=${VRAM_MODE:-'--lowvram'}
+warmup=$(echo ${WARMUP:-false} | tr '[:upper:]' '[:lower:]')
+device=${DEVICE:-0}
+port=${PORT:-6919}
+
+if [ -n "$vram_mode" ]
+then
+    python main.py $vram_mode --cuda-device $device --disable-xformers &
+else
+    python main.py --disable-xformers  --cuda-device $device &
+fi
+
+cleanup
